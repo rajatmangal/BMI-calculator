a273b4c64d0db9525e1f10e7b2ce62beccdb35ee
Rajat Mangal <rajatmangal@Rajats-MBP.hitronhub.home>
2019-07-04 22:18:30 -0700

    All cards added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,12 +13,67 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex:1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(Color(0XFF1D1E33)),
+
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0XFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex:1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(Color(0XFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex:1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(Color(0XFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0XFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  Color color;
+  ReusableCard(Color color) {
+    this.color = color;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: this.color,
+          borderRadius: BorderRadius.circular(10),
+        ),
     );
   }
 }
